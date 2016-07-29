db5640062d2f63f38ede6efd2f1baa6411cf8362
celinelee522 <celinelee522@gmail.com>
2016-07-29 15:07:31 +0900

    드라이버업로드
    아두이노 바꾸니까 업로드는 됨

---
+++ motor_shield/motor_shield.ino
@@ -0,0 +1,97 @@
+// Adafruit Motor shield library
+
+// copyright Adafruit Industries LLC, 2009
+
+// this code is public domain, enjoy!
+
+
+
+
+#include <AFMotor.h>
+
+#include <Servo.h> 
+
+
+
+
+
+Servo servo1;
+
+// Stepper motor on M3+M4 48 steps per revolution
+
+AF_Stepper stepper(48, 2);
+
+
+
+
+void setup() {
+
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+
+  Serial.println("Motor party!");
+
+  
+
+  // turn on servo
+
+  servo1.attach(9);
+  }
+
+
+
+
+int i;
+
+
+
+
+// Test the DC motor, stepper and servo ALL AT ONCE!
+
+void loop() {
+
+  for (i=0; i<255; i++) {
+
+    servo1.write(i);
+
+    stepper.step(2048, FORWARD, INTERLEAVE);
+
+    delay(3);
+
+ }
+
+ 
+
+  for (i=255; i!=0; i--) {
+
+    servo1.write(i-255);
+    
+    stepper.step(2048, BACKWARD, INTERLEAVE);
+
+    delay(3);
+
+ }
+
+ 
+  for (i=0; i<255; i++) {
+
+    servo1.write(i);
+
+    delay(3);
+
+    stepper.step(1, FORWARD, DOUBLE);
+
+ }
+
+ 
+
+  for (i=255; i!=0; i--) {
+
+    servo1.write(i-255);
+
+    stepper.step(1, BACKWARD, DOUBLE);
+
+    delay(3);
+
+ }
+
+}
